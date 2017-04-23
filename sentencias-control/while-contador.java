c50fc0eab4ff5c23960394058256b3a12b4e245a
victorcuervo <vcuervo@gmail.com>
2017-04-23 12:37:05 +0200

    Contador con un bucle while

---
+++ sentencias-control/while-contador.java
@@ -0,0 +1,15 @@
+/*
+  Ejemplo de uso de un bucle while para realizar un contador
+*/
+
+public class WhileContador {
+  public static void main (String[] args) {
+
+    int contador = 1;
+    while (contador <= 10) {
+      System.out.println(contador);
+      contador++;
+    }
+
+  }
+}
