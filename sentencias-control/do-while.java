0fa2e7e01a8a38f0b5a8978d75d708e03921d16a
victorcuervo <vcuervo@gmail.com>
2017-04-23 12:38:55 +0200

    Bucle do-while

---
+++ sentencias-control/do-while.java
@@ -0,0 +1,18 @@
+/*
+  Ejemplo de uso de un bucle while para realizar un contador
+*/
+
+public class WhileContador {
+  public static void main (String[] args) {
+
+    Scanner reader = new Scanner(System.in);
+    int iNumero;
+
+    do {
+    	System.out.println(“Introduce carácter por consola”);
+    	iNumero = reader.nextInt();
+    	System.out.println(iNumero);
+    } while (iNumero <> 0);
+
+  }
+}
