71ac8dbe6df5591db23a2407b111b20c6e1e3354
victorcuervo <vcuervo@gmail.com>
2017-04-23 12:16:58 +0200

    Usar una estructura if-then-else como si fuese un switch

---
+++ sentencias-control/if-como-switch.java
@@ -0,0 +1,39 @@
+/*
+  Usar una estructura if-then-else como si fuese un switch
+*/
+
+public class SentenciasIfThenElse {
+  public static void main (String[] args) {
+
+    if (iMes == 1){
+    	sMes = "Enero";
+    } else if (iMes == 2) {
+    	sMes = "Febrero";
+    } else if (iMes == 3) {
+    	sMes = "Marzo";
+    } else if (iMes == 4) {
+    	sMes = "Abril";
+    } else if (iMes == 5) {
+    	sMes = "Mayo";
+    } else if (iMes == 6) {
+    	sMes = "Junio";
+    } else if (iMes == 7) {
+    	sMes = "Julio";
+    } else if (iMes == 8) {
+    	sMes = "Agosto";
+    } else if (iMes == 9) {
+    	sMes = "Septiembre";
+    } else if (iMes == 10) {
+    	sMes = "Octubre";
+    } else if (iMes == 11) {
+    	sMes = "Noviembre";
+    } else if (iMes == 12) {
+    	sMes = "Diciembre";
+    } else {
+    	sMes = "Mes incorrecto";
+    }
+
+    System.out.println(sMes);
+
+  }
+}
