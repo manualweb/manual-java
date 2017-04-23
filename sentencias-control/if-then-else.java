57711b92341fce2d432fcea0f6b7c787e66f1df9
victorcuervo <vcuervo@gmail.com>
2017-04-23 12:06:21 +0200

    Ejemplo Sentencias if-then-else

---
+++ sentencias-control/if-then-else.java
@@ -0,0 +1,13 @@
+public class SentenciasIfThenElse {
+  public static void main (String[] args) {
+
+    int valor = 4;
+
+    if (valor < 10) {
+      System.out.println("El número es menor de 10");
+    } else {
+      System.out.println("El número es mayor de 10");
+    }
+
+  }
+}
